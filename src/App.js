9ecbfdc9b5a3454dfc5e51cfb4d2966a4d190f1a
konmaris <mariskon02@gmail.com>
2022-11-06 23:10:30 +0200

    created basic containers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const AppContainerStyle = {
+    width: "100%",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  const CenterContainerStyle = {
+    // backgroundColor: "#ececec",
+    width: "70%",
+    height: "70%",
+    borderRadius: "15px",
+    display: "flex",
+  };
+
+  const SidebarContainerStyle = {
+    backgroundColor: "#ffeaea",
+    width: "25%",
+    height: "100%",
+    borderRadius: "15px",
+    marginRight: "1%",
+  };
+
+  const ContentContainerStyle = {
+    backgroundColor: "#ffbbbb",
+    width: "74%",
+    height: "100%",
+    borderRadius: "15px",
+  };
+
+  const TitleStyle = {
+    margin: 0,
+    fontSize: 32,
+    marginLeft: 30,
+    marginTop: 20,
+    color: "#9c5d5d",
+    fontWeight: "500",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={AppContainerStyle}>
+      <div style={CenterContainerStyle}>
+        <div style={SidebarContainerStyle}>
+          <p style={TitleStyle}>Elena's life</p>
+        </div>
+        <div style={ContentContainerStyle}></div>
+      </div>
     </div>
   );
 }
